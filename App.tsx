00a5f86f182fe9d66048ab6e8b3b439d6089ee46
GuiQRSM <endbeob15@gmail.com>
2024-03-02 12:50:19 -0300

    Add settings style in tags componenets.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,30 @@
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 import { Fragment } from 'react';
 
 export default function App() {
   return (
-    <Fragment>
-    <Text>Imhere</Text>
-    <Text>App Building!</Text>
-    </Fragment>
+    <View style={{
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 24,
+    }}>
+
+    <Text style={{
+      color: '#FDFCFE',
+      fontSize: 24,
+      fontWeight: 'bold',
+      marginTop: 48,
+    }}>
+      Nome do evento
+    </Text>
+
+    <Text style={{
+      color: '#6B6B6B',
+      fontSize: 16,
+    }}>
+      Sexta, 8 de março de 2024.
+    </Text>
+    
+    </View>
   )
 }
