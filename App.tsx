d8d9c78d1f2b2af0708f30cc130b8bb997eafe2c
GuiQRSM <endbeob15@gmail.com>
2024-03-02 12:24:01 -0300

    Add an exportantion "Fragment" on the "App.tsx"

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
 import { Text } from 'react-native';
+import { Fragment } from 'react';
 
 export default function App() {
   return (
+    <Fragment>
     <Text>Imhere</Text>
+    <Text>App Building!</Text>
+    </Fragment>
   )
 }
