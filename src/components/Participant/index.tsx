774421d507e38ac6eb5a708db9bc5396c7d2aeb5
GuiQRSM <endbeob15@gmail.com>
2024-03-07 23:28:25 -0300

    Finishes the config of the component 'Participant'

--- src/components/Participant/index.tsx
+++ src/components/Participant/index.tsx
@@ -1,12 +1,22 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity} from "react-native";
 import { styles } from "./style";
 
 export function Participant() {
     return (
     <View style={styles.container}>
+
         <Text style={styles.name}>
             Guilherme
         </Text>
+
+    <TouchableOpacity style={styles.buttton}>
+
+      <Text style={styles.buttonText}>
+       -
+      </Text>
+
+    </TouchableOpacity>
+
     </View>
     );
 }
