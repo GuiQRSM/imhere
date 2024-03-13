ffa839334864d53adf43fa02fe485234e2e1f790
GuiQRSM <endbeob15@gmail.com>
2024-03-12 23:09:52 -0300

    Setting the style configs for the StatusBar.

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
 import { StatusBar } from 'react-native';
 import { Home } from './src/screens/Home';
 
-
 export default function App() {
   return (
-   <>
-    <StatusBar />
-    <Home />
-   </> 
-   
+    <>
+       <StatusBar 
+       barStyle="light-content"
+       />
+      <Home />
+    </>   
   );
 }
 
