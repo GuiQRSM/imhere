95fba0309d69099a4e66ff8fe5836aac7a7da897
GuiQRSM <endbeob15@gmail.com>
2024-03-12 23:12:01 -0300

    Finishes the configs for the 'StatusBar'

--- App.tsx
+++ App.tsx
@@ -4,8 +4,10 @@ import { Home } from './src/screens/Home';
 export default function App() {
   return (
     <>
-       <StatusBar 
-       barStyle="light-content"
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
        />
       <Home />
     </>   
